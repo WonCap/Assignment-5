1bae7b928ff3fd48e1560660f4ef3aaf7ab45279
WonCap <capsulewonder@gmail.com>
2025-05-26 22:47:31 +0530

    Add files via upload

---
+++ Dictionary of Student Marks.py
@@ -0,0 +1,8 @@
+sheet = {'Henry':60,'Peter':90,'Jake':75}
+a = input("Enter the student's name: ")
+if a not in sheet:
+    print('Student not found.')
+else:
+    print(f"{a}'s marks: {sheet[a]}")
+
+
---
+++ List Slicing.py
@@ -0,0 +1,5 @@
+l = [1,2,3,4,5,6,7,8,9,10]
+
+print(f"Original List: {l}")
+print(f"Extracted first five elements: {l[0:5]}")
+print(f"Reverse extracted Elements: {l[4::-1]}")
